4db884875a610c4603f6c36d1f7fb231fa5e80d4
cxsquared <cxsquared@gmail.com>
2020-11-08 16:05:24 -0500

    Trying to add share button

--- js/src/forum/addShareControl.js
+++ js/src/forum/addShareControl.js
@@ -1,18 +1,18 @@
 import { extend } from 'flarum/extend';
 import app from 'flarum/app';
 import PostControls from 'flarum/utils/PostControls';
+import Button from 'flarum/components/Button';
+import ClipboardJS from 'clipboard';
 
 export default function() {
+    new ClipboardJS('#comment-share');
+
     extend(PostControls, 'userControls', function(items, post) {
-        const touch = 'ontouchstart' in document.documentElement;
         const permalink = window.location.origin + app.route.post(post);
 
-        const item = touch ?
-            <a className="Button PostMeta-permalink" href={permalink}>{permalink}</a> : 
-            <input className="Formcontrol PostMeta-permalink" value={permalink} onclick={(e) => e.stopPropagation()} />;
-
         items.add('share',
-            item
+            <Button icon="fas fa-share" id="comment-share" dataClipboardText={permalink}>Copy Link</Button>,
+            5
         );
     });
 }
